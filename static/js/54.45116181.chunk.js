a00e714916c4df301ae58fe04458069b04e20f1d
Dino-Martinez <>
2021-01-31 22:25:46 -0800

    Updates

---
+++ static/js/54.45116181.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpionic-react"]=this["webpackJsonpionic-react"]||[]).push([[54],{116:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(9),o=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.h)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var c=o.closest("ion-content");c&&c.componentOnReady().then((function(){Object(i.f)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=54.45116181.chunk.js.map
